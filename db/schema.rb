23435f62a54cd0afbcfc5c4c6dbf1face257b023
Jules Roman Polidario <jrpolidario@yahoo.com>
2016-05-21 23:58:47 +0000

    command: rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160521233331) do
+ActiveRecord::Schema.define(version: 20160521235444) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20160521233331) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "category"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
